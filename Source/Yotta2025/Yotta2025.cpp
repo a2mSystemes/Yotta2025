02356d3ffba3189850a2084185138491b364f7c2
Ange-Marie MAURIN <a-m.maurin@a2msystemes.fr>
2025-03-12 13:43:58 +0100

    first local commit

---
+++ Source/Yotta2025/Yotta2025.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Yotta2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Yotta2025, "Yotta2025" );
